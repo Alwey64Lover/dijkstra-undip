13e2b8bdc20bd54c85734021e8c2ecf61809a3af
ShiranaiZo <faishalbariqmaulana123@gmail.com>
2024-09-24 18:05:10 +0700

    feat: [fm] add user seeder per role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,9 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Lecturer;
+use App\Models\Student;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 
 class UserSeeder extends DatabaseSeeder
 {
@@ -14,45 +13,120 @@ class UserSeeder extends DatabaseSeeder
      */
     public function run(): void
     {
-        $data = [
-            $this->data([
-                'name' => 'Admin',
-                'email' => 'admin@gmail.com',
-                'password' => bcrypt('password'),
-                'role' => 'superadmin',
-            ]),
-            $this->data([
-                'name' => 'Clement Ponso',
-                'email' => 'clementponso@deans.undip.ac.id',
-                'password' => bcrypt('password'),
-                'role' => 'dean',
-            ]),
-            $this->data([
-                'name' => 'Puji Widodo Sutantio',
-                'email' => 'pujiwidodosut@informatics.undip.ac.id',
-                'password' => bcrypt('password'),
-                'role' => 'head_of_department',
-            ]),
-            $this->data([
-                'name' => 'Singh Khir Khan',
-                'email' => 'singkhirkhan@operator.undip.ac.id',
-                'password' => bcrypt('password'),
-                'role' => 'academic_division',
-            ]),
-            $this->data([
-                'name' => 'John Doe',
-                'email' => 'johndoe@lecturer.undip.ac.id',
-                'password' => bcrypt('password'),
-                'role' => 'lecturer',
-            ]),
-            $this->data([
-                'name' => 'Daffa Fairuz Annizari',
-                'email' => 'dafairuz@students.undip.ac.id',
-                'password' => bcrypt('password'),
-                'role' => 'student',
-            ])
-        ];
+        // Lecturers & Students
+            $lecturers = [
+                $this->data([
+                    'name' => 'John Doe',
+                    'email' => 'johndoe@lecturer.undip.ac.id',
+                    'password' => bcrypt('password'),
+                    'role' => 'lecturer',
+                ]),
+                $this->data([
+                    'name' => 'Dr.Eng. Adi Wibowo, S.Si., M.Kom.',
+                    'email' => 'adiwibowo@lecturer.undip.ac.id',
+                    'password' => bcrypt('password'),
+                    'role' => 'lecturer',
+                ]),
+                $this->data([
+                    'name' => 'Etna Vianita, S.Mat., M.Mat.',
+                    'email' => 'etna@lecturer.undip.ac.id',
+                    'password' => bcrypt('password'),
+                    'role' => 'lecturer',
+                ]),
+                $this->data([
+                    'name' => 'Sandy Kurniawan, S.Kom., M.Kom.',
+                    'email' => 'sandy@lecturer.undip.ac.id',
+                    'password' => bcrypt('password'),
+                    'role' => 'lecturer',
+                ]),
+                $this->data([
+                    'name' => 'Clement Ponso',
+                    'email' => 'clementponso@deans.undip.ac.id',
+                    'password' => bcrypt('password'),
+                    'role' => 'dean',
+                ]),
+                $this->data([
+                    'name' => 'Alfonso Rakabuming Putra Widodo',
+                    'email' => 'fufufafa@informatics.undip.ac.id',
+                    'password' => bcrypt('password'),
+                    'role' => 'head_of_department',
+                ]),
+            ];
 
-        User::insert($data);
+            $students = [
+                $this->data([
+                    'name' => 'Daffa Fairuz Annizari',
+                    'email' => 'dafairuz@students.undip.ac.id',
+                    'password' => bcrypt('password'),
+                    'role' => 'student',
+                ]),
+            ];
+
+        // Lecturer & Student Details
+            $lecturerDetails = [
+                $this->data([
+                    'user_id' => $lecturers[0]['id'],
+                    'nip' => '198203092006041001',
+                    'nidn' => null,
+                ]),
+                $this->data([
+                    'user_id' => $lecturers[1]['id'],
+                    'nip' => '198203092006041002',
+                    'nidn' => '0001047404',
+                ]),
+                $this->data([
+                    'user_id' => $lecturers[2]['id'],
+                    'nip' => '197404011999031003',
+                    'nidn' => null,
+                ]),
+                $this->data([
+                    'user_id' => $lecturers[3]['id'],
+                    'nip' => '197404011999031003',
+                    'nidn' => '0001047405',
+                ]),
+                $this->data([
+                    'user_id' => $lecturers[4]['id'],
+                    'nip' => '197404011999031003',
+                    'nidn' => '0001047406',
+                ]),
+                $this->data([
+                    'user_id' => $lecturers[5]['id'],
+                    'nip' => '197404011999031003',
+                    'nidn' => '0001047407',
+                ])
+            ];
+
+            $studentDetails = [
+                $this->data([
+                    'user_id' => $students[0]['id'],
+                    'academic_advisor_id' => $lecturerDetails[3]['id'],
+                    'nim' => '24060122140044',
+                    'year' => 2022,
+                ])
+            ];
+
+        // Users & Merge
+            $users = [
+                $this->data([
+                    'name' => 'Admin',
+                    'email' => 'admin@gmail.com',
+                    'password' => bcrypt('password'),
+                    'role' => 'superadmin',
+                ]),
+                $this->data([
+                    'name' => 'Singh Khir Khan',
+                    'email' => 'singkhirkhan@operator.undip.ac.id',
+                    'password' => bcrypt('password'),
+                    'role' => 'academic_division',
+                ]),
+            ];
+
+            $users = array_merge($users, $lecturers, $students);
+
+        // Insert
+            User::insert($users);
+            Lecturer::insert($lecturerDetails);
+            Student::insert($studentDetails);
+
     }
 }

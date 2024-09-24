4bbcbecefc62bf1cbcaceab30906aa171792fb3d
ShiranaiZo <faishalbariqmaulana123@gmail.com>
2024-09-24 10:25:09 +0700

    feat: [fm] add seeder account per role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,19 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
+    public function data(array $data): array
+    {
+        return array_merge($data, [
+            'id' => @$data['id'] ?: strtolower(Str::ulid()),
+            'created_at' => now()->toDateTimeString(),
+            'updated_at' => now()->toDateTimeString(),
+        ]);
+    }
+
     /**
      * Seed the application's database.
      */
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,24 +2,57 @@
 
 namespace Database\Seeders;
 
+use App\Models\Lecturer;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class UserSeeder extends Seeder
+class UserSeeder extends DatabaseSeeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        User::create(
-            [
+        $data = [
+            $this->data([
                 'name' => 'Admin',
                 'email' => 'admin@gmail.com',
                 'password' => bcrypt('password'),
                 'role' => 'superadmin',
-            ]
-        );
+            ]),
+            $this->data([
+                'name' => 'Clement Ponso',
+                'email' => 'clementponso@deans.undip.ac.id',
+                'password' => bcrypt('password'),
+                'role' => 'dean',
+            ]),
+            $this->data([
+                'name' => 'Puji Widodo Sutantio',
+                'email' => 'pujiwidodosut@informatics.undip.ac.id',
+                'password' => bcrypt('password'),
+                'role' => 'head_of_department',
+            ]),
+            $this->data([
+                'name' => 'Singh Khir Khan',
+                'email' => 'singkhirkhan@operator.undip.ac.id',
+                'password' => bcrypt('password'),
+                'role' => 'academic_division',
+            ]),
+            $this->data([
+                'name' => 'John Doe',
+                'email' => 'johndoe@lecturer.undip.ac.id',
+                'password' => bcrypt('password'),
+                'role' => 'lecturer',
+            ]),
+            $this->data([
+                'name' => 'Daffa Fairuz Annizari',
+                'email' => 'dafairuz@students.undip.ac.id',
+                'password' => bcrypt('password'),
+                'role' => 'student',
+            ])
+        ];
+
+        User::insert($data);
     }
 }

905fe6d494a9480befe9c9ad67c846051629d5b9
Bravo-Alfon <sutancs42@gmail.com>
2024-09-27 14:08:51 +0700

    Fix: [as] Fix the backgroung image to fit size of view

--- resources/views/layouts/backend/auth-app.blade.php
+++ resources/views/layouts/backend/auth-app.blade.php
@@ -2,6 +2,13 @@
 
 @push('css')
     <link rel="stylesheet" href="{{ asset('assets/compiled/css/auth.css') }}">
+    <style>
+        #auth-right img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    </style>
 @endpush
 
 @section('body')
@@ -13,9 +20,9 @@
                 </div>
             </div>
 
-            <div class="col-lg-7 d-none d-lg-block" style="height: 100%">
-                <div id="auth-right" style="overflow: hidden;">
-                    <img src="{{ asset('storage/static/fsm.jpg') }}" alt="" >
+            <div class="col-lg-7 d-none d-lg-block" style="height: 100%;">
+                <div id="auth-right">
+                    <img src="{{ asset('storage/static/fsm.jpg') }}" alt="Responsive image">
                 </div>
             </div>
         </div>
